d5e4664d6df4872b20f8e7905cf74ef30c326deb
Salimah <salimaaomari@gmail.com>
2021-01-20 16:04:15 -0800

    new CRUD method

--- movies/moviesapi/api/views.py
+++ movies/moviesapi/api/views.py
@@ -19,69 +19,78 @@ class users(viewsets.ModelViewSet):
         users=User.objects.all()
         return users
 
-@api_view(['GET'])
-def movie_list(request):
-    movies=movie.objects.all()
-    serializer=movieSerializer(movies,many=True)
-    return Response(serializer.data)
-
-@api_view(['GET'])
-def movie_detail(request,pk):
-    movies=movie.objects.get(id=pk)
-    serializer=movieSerializer(movies,many=False)
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def movie_create(request):
-    serializer=movieSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
-
-@api_view(['POST'])
-def movie_update(request,pk):
-    movies=movie.objects.get(id=pk)
-    serializer=movieSerializer(instance=movies, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data) 
-
-@api_view(['DELETE'])
-def movie_delete(request,pk):
-    movies=movie.objects.get(id=pk)
-    movies.delete()
-    return Response({'message':'successfuly deleted'})
+class movies(viewsets.ModelViewSet):
+    queryset=movie.objects.all()
+    serializer_class = movieSerializer
 
 
-@api_view(['GET'])
-def rental_list(request):
-    rental=rentals.objects.all()
-    serializer=rentalserializer(rental,many=True)
-    return Response(serializer.data)
 
-@api_view(['GET'])
-def rental_detail(request,pk):
-    rental=rentals.objects.get(id=pk)
-    serializer=rentalserializer(rental,many=False)
-    return Response(serializer.data)
 
-@api_view(['POST'])
-def create_rental(request):
-    serializer=rentalserializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
 
-@api_view(['POST'])
-def update_rental(request,pk):
-    rental=rentals.objects.get(id=pk)
-    serializer=rentalserializer(instance=rental, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data) 
 
-@api_view(['DELETE'])
-def delete_rental(request,pk):
-    rental=rentals.objects.get(id=pk)
-    rental.delete()
-    return Response({'message':'successfuly deleted'}) 
+# @api_view(['GET'])
+# def movie_list(request):
+#     movies=movie.objects.all()
+#     serializer=movieSerializer(movies,many=True)
+#     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def movie_detail(request,pk):
+#     movies=movie.objects.get(id=pk)
+#     serializer=movieSerializer(movies,many=False)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def movie_create(request):
+#     serializer=movieSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def movie_update(request,pk):
+#     movies=movie.objects.get(id=pk)
+#     serializer=movieSerializer(instance=movies, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data) 
+
+# @api_view(['DELETE'])
+# def movie_delete(request,pk):
+#     movies=movie.objects.get(id=pk)
+#     movies.delete()
+#     return Response({'message':'successfuly deleted'})
+
+
+# @api_view(['GET'])
+# def rental_list(request):
+#     rental=rentals.objects.all()
+#     serializer=rentalserializer(rental,many=True)
+#     return Response(serializer.data)
+
+# @api_view(['GET'])
+# def rental_detail(request,pk):
+#     rental=rentals.objects.get(id=pk)
+#     serializer=rentalserializer(rental,many=False)
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def create_rental(request):
+#     serializer=rentalserializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data)
+
+# @api_view(['POST'])
+# def update_rental(request,pk):
+#     rental=rentals.objects.get(id=pk)
+#     serializer=rentalserializer(instance=rental, data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#     return Response(serializer.data) 
+
+# @api_view(['DELETE'])
+# def delete_rental(request,pk):
+#     rental=rentals.objects.get(id=pk)
+#     rental.delete()
+#     return Response({'message':'successfuly deleted'}) 
